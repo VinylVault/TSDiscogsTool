563efaa35a98c7ad6612b38e3678940761fa653b
Dex Vinyl <studio@thevinylvaultshow.co.uk>
2022-01-29 10:19:53 +0000

    All relevent GET endpoints are now available - No Search .. will be in Search Module - No Marketplace .. will be in Shop Module as they are limited to Stores

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ console.log("Bollocks, This Works, I'm A Software Engineer!");
 
 const main = async () => {
   const discogs = new Discogs({});
-  const data = await discogs.getArtist("72872");
+
+  //USER INFORMATION
   const user = await discogs.getUser("DexVinyl");
   const collection = await discogs.getUserCollection("DexVinyl");
   const wantlist = await discogs.getUserWantlist("DexVinyl");
@@ -15,15 +16,47 @@ const main = async () => {
   );
   const collectionValue = await discogs.getUserCollectionValue("DexVinyl");
 
-  console.log(data.data);
-  console.log(user.data);
-  console.log(folders.data);
-  console.log(collection.data);
-  console.log(wantlist.data);
-  console.log(folderContents.data);
-  console.log(collectionValue.data);
-  console.log(collectionValue.headers);
+  // RELEASE INFORMATION
 
+  const releaseDetails = await discogs.getRelease("8749543"); //RICK-ROLL
+  const releaseUserRating = await discogs.getReleaseUserRating("8749543");
+  const releaseCommunityRating = await discogs.getReleaseCommunityRating(
+    "8749543"
+  );
+  const releaseStats = await discogs.getReleaseStats("8749543");
+  const releaseMaster = await discogs.getMasterRelease("83725");
+  const releaseMasterVersions = await discogs.getMasterReleaseVersions("83725");
+
+  // ARTIST INFORMATION
+
+  const artist = await discogs.getArtist("72872"); //RICK ASTLEY
+  const artistReleases = await discogs.getArtistReleases("72872");
+
+  // LABEL INFORMATION
+
+  const label = await discogs.getLabel("895"); //RCA
+  const labelReleases = await discogs.getLabelReleases("895");
+
+  // DISPLAY SHIT ON SCREEN TO TEST
+
+  //console.log(artist.data);
+  //   console.log(user.data);
+  //   console.log(folders.data);
+  //   console.log(collection.data);
+  //   console.log(wantlist.data);
+  //   console.log(folderContents.data);
+  //   console.log(collectionValue.data);
+  //   console.log(releaseDetails.data);
+  //   console.log(releaseDetails.data);
+  //   console.log(releaseUserRating.data);
+  //   console.log(releaseCommunityRating.data);
+  //   console.log(releaseStats.data);
+  //   console.log(releaseMaster.data);
+  //   console.log(releaseMasterVersions.data);
+  //   console.log(artistDetails.data);
+  //   console.log(artistReleases.data);
+  //   console.log(labelDetails.data);
+  //   console.log(labelReleases.data);
   console.log(discogs.getRatelimit());
 };
 
