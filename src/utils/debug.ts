fff7c0b7a5fb48ca4aa78ca78d5c91e3126343e3
Mike Elsmore <mike@elsmore.me>
2022-03-21 20:01:49 +0000

    Add the helpful debuggar (@finitesingularity joke) to the codebase

---
+++ src/utils/debug.ts
@@ -0,0 +1,7 @@
+import Debug from 'debug';
+
+export const info = Debug('discogs:info');
+export const warn = Debug('discogs:warn');
+export const error = Debug('discogs:error');
+export const debug = Debug('discogs:debug');
+export const log = Debug('discogs:log');
